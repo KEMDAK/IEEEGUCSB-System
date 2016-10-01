59d5ed96c62b2644779d47860290d57c2134eaf4
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-10-01 22:36:53 +0200

    added the server configurations to be able to run it

---
+++ config/database/Database.js
@@ -0,0 +1,33 @@
+var mysql = require('mysql');
+var Sequelize = require('sequelize');
+
+/* Connecting to the database. */
+var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,
+{
+    host: 'localhost',
+    dialect: 'mysql',
+    port:    3306,
+    logging: (process.env.SQL_LOG === 'true')? console.log : false,
+    define: {
+        charset: 'utf8'
+    }
+});
+
+module.exports.initialize = function(callback) {
+
+    /* define the models */
+
+    /* defining relation */
+    require('./Relations');
+
+
+    var force = (process.env.RESET_DB === 'true')? true : false;
+
+    sequelize.sync({ force: force }).then(function(err) {
+        /* seeding */
+
+        callback();
+    }, function (err) {
+        callback(err);
+    });
+};
