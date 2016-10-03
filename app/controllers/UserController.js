68295a6fd778d1d185b60f34934ef4a1940c3485
soofyhbk2007 <mastergame_2007@hotmail.com>
2016-10-03 13:44:21 +0200

    almost finished show function in user controller

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -7,12 +7,12 @@
 var User = require('../models/User').User;
 
 /**
- * This function gets a list of all users currently in the database.
- * @param  {HTTP}   req  The request object
- * @param  {HTTP}   res  The response object
- * @param  {Function} next Callback function that is called once done with handling the request
- * @return {Array}  result The users currently in the database
- */
+* This function gets a list of all users currently in the database.
+* @param  {HTTP}   req  The request object
+* @param  {HTTP}   res  The response object
+* @param  {Function} next Callback function that is called once done with handling the request
+* @return {Array}  result The users currently in the database
+*/
 module.exports.index = function(req, res, next) {
 
    User.findAll({}).then(function(users) {
@@ -62,4 +62,73 @@ module.exports.index = function(req, res, next) {
 
       next();
    });
+};
+
+module.exports.show = function(req, res, next) {
+   /** validate the id attached with the req with forms */
+
+   var id = req.params.id;
+   var user = req.user;
+
+   User.findById(id).then(function(requestedUser) {
+      /** Get the committee of the requested user */
+      requestedUser.getCommittee().then(function(requestedCommittee) {
+
+         /** Get the committee of logged in user */
+         user.getCommittee().then(function(myCommittee) {
+
+            var result;
+            /** Return the detailed profile only if the requesting user is an upper board or a high board trying to view
+                one of his member's profile or if the rquesting user of trying to view his own profile.
+             */
+            if ((user.isHighBoard() && myCommittee.id == requestedCommittee.id) || user.isUpperBoard() || user.id == id) {
+               // detailed info
+            }
+            else {
+               // basic info
+            }
+
+            res.status(200).json({
+               status:'succeeded',
+               user: result
+            });
+
+            next();
+
+         }).catch(function(err) {
+            /* failed to find my committee.*/
+            res.status(500).json({
+               status:'failed',
+               message: 'Internal server error'
+            });
+
+            req.err = err;
+
+            next();
+         });
+
+      }).catch(function(err) {
+         /* failed to find the user's committee.*/
+         res.status(500).json({
+            status:'failed',
+            message: 'Internal server error'
+         });
+
+         req.err = err;
+
+         next();
+      });
+
+   }).catch(function(err) {
+      /* failed to find the user in the database */
+      res.status(500).json({
+         status:'failed',
+         message: 'Internal server error'
+      });
+
+      req.err = err;
+
+      next();
+   });
+
 };
