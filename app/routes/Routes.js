260ec351d7fed1ca2fe065a4a203c5c42a0b7e7f
Kareem Mokhtar <kareem-mokhtar@outlook.com>
2017-02-24 18:41:11 +0200

    added trusted headers

--- app/routes/Routes.js
+++ app/routes/Routes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
     app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', "http://localhost:" + process.env.PORT);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'User_Agent, Authorization');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, User_Agent, Authorization');
 
         next();
     });
