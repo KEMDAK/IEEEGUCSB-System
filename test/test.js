d061cebd0d6178c367219c6f4904789af5f832e3
soofyhbk2007 <mastergame_2007@hotmail.com>
2017-02-27 02:40:28 +0200

    added more meeting controller tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
-var app, sq;
+var app, sq, data;
 
 before(function(done) {
+   process.env.ENV = 'test';
    require('../server')(function(MyApp, MySq, err) {
       if (err) {
          console.log('Unable to start Tests.');
@@ -10,6 +11,7 @@ before(function(done) {
          console.log('Starting Tests...');
          app = MyApp;
          sq = MySq;
+         data = require('./data.js')();
          done();
       }
    });
@@ -18,7 +20,6 @@ before(function(done) {
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var should = chai.should();
-var data = require('./data.js')();
 
 chai.use(chaiHttp);
 
@@ -26,7 +27,6 @@ var User, Meeting, Committee, Identity;
 
 describe('Meeting Controller', function() {
    before(function(done) {
-      this.timeout(10000);
       sq.sync({ force: true }).then(function() {
 
          /* Require models */
@@ -54,9 +54,13 @@ describe('Meeting Controller', function() {
    });
 
    describe('POST /api/meeting', function(done) {
+      /************************
+      * Authentication Tests *
+      ************************/
       it('Should not allow a visitor to add a meeting.', function(done) {
          chai.request(app)
          .post('/api/meeting')
+         .set('User_Agent', 'Web')
          .end(function(err, res) {
             try {
                res.should.have.status(401);
@@ -72,6 +76,7 @@ describe('Meeting Controller', function() {
       it('Should not allow a Member to add a meeting.', function(done) {
          chai.request(app)
          .post('/api/meeting')
+         .set('User_Agent', 'Web')
          .set('Authorization', data.identities[7].token)
          .end(function(err, res) {
             try {
@@ -85,6 +90,62 @@ describe('Meeting Controller', function() {
          });
       });
 
+      it('Should deny access due to missing User Agent header.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            end_date: "2017-2-25 10:00:00",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            location: "Location",
+            description: "Description",
+            attendees: [2, 3, 4]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(401);
+               res.body.should.have.property('status').and.equal('failed');
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should deny access due to invalid User Agent header.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            end_date: "2017-2-25 10:00:00",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            location: "Location",
+            description: "Description",
+            attendees: [2, 3, 4]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Windows Phone')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(401);
+               res.body.should.have.property('status').and.equal('failed');
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      /********************
+      * Validation Tests *
+      ********************/
       it('Should not allow a High Board to add a meeting with members not from his/her committee.', function(done) {
          var meeting = {
             start_date: "2017-2-25 08:00:00",
@@ -97,12 +158,289 @@ describe('Meeting Controller', function() {
 
          chai.request(app)
          .post('/api/meeting')
+         .set('User_Agent', 'Web')
          .set('Authorization', data.identities[3].token)
          .send(meeting)
          .end(function(err, res) {
             try {
-               res.should.have.status(401);
+               res.should.have.status(400);
                res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to missing \'start_date\' parameter in the body.', function(done) {
+         var meeting = {
+            end_date: "2017-2-25 10:00:00",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            location: "Location",
+            description: "Description",
+            attendees: [9, 13]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to invalid \'start_date\' parameter in the body.', function(done) {
+         var meeting = {
+            start_date: "This is an invalid start_date",
+            end_date: "2017-2-25 10:00:00",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            location: "Location",
+            description: "Description",
+            attendees: [9, 13]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to missing \'end_date\' parameter in the body.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            location: "Location",
+            description: "Description",
+            attendees: [9, 13]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to invalid \'end_date\' parameter in the body.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            end_date: "This is an invalid end_date",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            location: "Location",
+            description: "Description",
+            attendees: [9, 13]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to missing \'goals\' parameter in the body.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            end_date: "2017-2-25 10:00:00",
+            location: "Location",
+            description: "Description",
+            attendees: [9, 13]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to invalid \'goals\' parameter in the body.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            end_date: "2017-2-25 10:00:00",
+            goals: "Goal 1",
+            location: "Location",
+            description: "Description",
+            attendees: [9, 13]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to missing \'location\' parameter in the body.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            end_date: "2017-2-25 10:00:00",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            description: "Description",
+            attendees: [9, 13]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to invalid \'location\' parameter in the body.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            end_date: "2017-2-25 10:00:00",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            location: 1,
+            description: "Description",
+            attendees: [9, 13]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to missing \'attendees\' parameter in the body.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            end_date: "2017-2-25 10:00:00",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            location: "Location",
+            description: "Description",
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
+               should.exist(err);
+               done();
+            } catch(error) {
+               done(error);
+            }
+         });
+      });
+
+      it('Should not allow the meeting to be added due to invalid \'attendees\' parameter in the body.', function(done) {
+         var meeting = {
+            start_date: "2017-2-25 08:00:00",
+            end_date: "2017-2-25 10:00:00",
+            goals: ["Goal 1", "Goal 2", "Goal 3"],
+            location: "Location",
+            description: "Description",
+            attendees: ["This", "is", "invalid"]
+         };
+
+         chai.request(app)
+         .post('/api/meeting')
+         .set('User_Agent', 'Web')
+         .set('Authorization', data.identities[0].token)
+         .send(meeting)
+         .end(function(err, res) {
+            try {
+               res.should.have.status(400);
+               res.body.should.have.property('status').and.equal('failed');
+               res.body.should.have.property('errors');  // TODO: Test the errors themselves
                should.exist(err);
                done();
             } catch(error) {
