ddf8798f6e89defa065373765761062282d99b88
Kareem Mokhtar <kareem-mokhtar@outlook.com>
2017-03-04 01:58:40 +0200

    testing travis ci

--- scripts/.travis.install-mysql-5.7.sh
+++ scripts/.travis.install-mysql-5.7.sh
@@ -4,13 +4,6 @@ sudo apt-get autoremove
 sudo apt-get autoclean
 sudo rm -rf /var/lib/mysql
 sudo rm -rf /var/log/mysql
-echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo dpkg --install mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
 sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
-set -x
-set -e
-sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
-sudo service mysql restart
+sudo apt-get install mysql-server -y
 mysql --version
