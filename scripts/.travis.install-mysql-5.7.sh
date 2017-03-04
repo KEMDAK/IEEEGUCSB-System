598bd0c9a099cfa51f4bd7d2df958355239a52db
Kareem Mokhtar <kareem-mokhtar@outlook.com>
2017-03-04 02:18:41 +0200

    testing travis ci

--- scripts/.travis.install-mysql-5.7.sh
+++ scripts/.travis.install-mysql-5.7.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-sudo apt-get remove --purge "^mysql.*"
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo rm -rf /var/lib/mysql
-sudo rm -rf /var/log/mysql
 echo mysql-apt-config mysql-apt-config/select-server select mysql-5.7 | 
 sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
-sudo dpkg -i mysql-apt-config_0.6.0-1_all.deb
+wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
+sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
 sudo apt-get update -q
-sudo apt-get install mysql-server -y
-sudo service mysql start
+sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
+sudo mysql_upgrade
 mysql --version
