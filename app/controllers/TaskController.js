8a57fe725bc532a76377d05e8bbb63d25f5e2e0c
Muhammad Khattab <msskmohammed96@gmail.com>
2017-03-12 10:26:51 +0200

    adding task.update and fixing some errors

--- app/controllers/TaskController.js
+++ app/controllers/TaskController.js
@@ -261,8 +261,7 @@ module.exports.store = function(req, res, next) {
          priority: req.body.priority,
          status: 'New',
          evaluation: req.body.evaluation,
-         supervisor: req.user.id,
-         assignedUsers: assigned_users
+         supervisor: req.user.id
       };
 
       Task.create(attributes).then(function(task)
@@ -322,6 +321,184 @@ module.exports.store = function(req, res, next) {
       });
    }
    rest();
+};
+
+/**
+* This function updates a task's information in the database
+* @param  {HTTP}   req  The request object
+* @param  {HTTP}   res  The response object
+* @param  {Function} next Callback function that is called once done with handling the request
+*/
+module.exports.update = function(req, res, next) {
+   var attributes = {};
+
+   /*Validate and sanitizing ID Input*/
+   req.checkParams('id', 'required').notEmpty();
+   req.sanitizeParams('id').escape();
+   req.sanitizeParams('id').trim();
+   req.checkParams('id', 'validity').isInt();
+
+   /*Validate and sanitizing title Input*/
+   req.checkBody('title', 'required').notEmpty();
+   req.sanitizeBody('title').escape();
+   req.sanitizeBody('title').trim();
+
+   /*Validate and sanitizing description Input*/
+   req.sanitizeBody('description').escape();
+   req.sanitizeBody('description').trim();
+
+   /*Validate and sanitizing deadline Input*/
+   req.checkBody('deadline', 'required').notEmpty();
+   req.checkBody('deadline', 'validity').isDate();
+   req.sanitizeBody('deadline').escape();
+   req.sanitizeBody('deadline').trim();
+
+   /*Validate and sanitizing end date Input*/
+   req.checkBody('priority', 'required').notEmpty();
+   req.sanitizeBody('priority').escape();
+   req.sanitizeBody('priority').trim();
+
+   var p = req.body.priority;
+   if(p!=1 && p!= 3&& p!= 5&& p!= 8)
+   {
+      // error
+   }
+
+   /*Validate and sanitizing end date Input*/
+   if(req.body.evaluation)
+   {
+      req.sanitizeBody('evaluation').escape();
+      req.sanitizeBody('evaluation').trim();
+   }
+   else
+   {
+      req.body.evaluation = null;
+   }
+
+   var rest = function(){
+      /*sending validation errors*/
+      errors = format(errors);
+      if (errors) {
+         /* input validation failed */
+         res.status(400).json({
+            status: 'failed',
+            errors: errors
+         });
+
+         req.err = 'TaskController.js, Line: 414\nSome validation errors occurred.\n' + JSON.stringify(errors);
+         next();
+         return;
+      }
+
+      Task.findById(req.params.id).then(function(task) {
+         if(!task) {
+            res.status(404).json({
+               status:'failed',
+               message: 'The requested route was not found.'
+            });
+
+            req.err = 'TaskController.js, Line: 438\nThe requested task was not found in the database or the user has no authority to edit it.';
+            next();
+         }
+         else if(task.supervisor != req.user.id) {
+            /* The requesting user has no authority to update the task */
+            res.status(403).json({
+               status:'failed',
+               message: 'Access denied'
+            });
+
+            req.err = 'TaskController.js, Line: 449\nThe requesting user has no authority to update the task.';
+            next();
+         }
+         else {
+            task.update(attributes).then(function(task) {
+               if(req.body.assignedUsers){
+                  task.setAssignedUsers(req.body.assignedUsers).then(function(){
+                     res.status(200).json({
+                        status: 'succeeded',
+                        message: 'task successfully updated'
+                     });
+
+                     next();
+                  }).catch(function(err) {
+                     /* failed to update the task assignedUsers in the database */
+                     res.status(500).json({
+                        status:'failed',
+                        message: 'Internal server error'
+                     });
+
+                     req.err = 'TaskController.js, Line: 470\nfailed to update the task assignedUsers in the database.\n' + String(err);
+                     next();
+                  });
+               } else {
+                  res.status(200).json({
+                     status: 'succeeded',
+                     message: 'task successfully updated'
+                  });
+
+                  next();
+               }
+            }).catch(function(err) {
+               /* failed to update the task in the database */
+               res.status(500).json({
+                  status:'failed',
+                  message: 'Internal server error'
+               });
+
+               req.err = 'TaskController.js, Line: 489\nCouldn\'t update the task in the database.\n' + String(err);
+
+               next();
+            });
+         }
+      }).catch(function(err) {
+         /* failed to find the task in the database */
+         res.status(500).json({
+            status:'failed',
+            message: 'Internal server error'
+         });
+
+         req.err = 'TaskController.js, Line: 501\nCouldn\'t find the task in the database.\n' + String(err);
+
+         next();
+      });
+   };
+
+   var errors = req.validationErrors();
+   if(req.body.assignedUsers){
+      /*validating the user list*/
+      User.findAll({ where: { id: { in: req.body.assignedUsers } } }).then(function(assignedUsers) {
+         req.checkBody('assignedUsers', 'validity').isArray(assignedUsers.length);
+         errors = req.validationErrors();
+         for (var i = 0; i < assignedUsers.length; i++) {
+            if(!req.user.isAdmin() && !req.user.isUpperBoard() && req.user.committee_id != assignedUsers[i].committee_id || req.user.id == assignedUsers[i].id){
+               if(!errors){
+                  errors = [];
+               }
+
+               errors.push({
+                  param: 'assignedUsers',
+                  value: req.body.assignedUsers,
+                  msg: 'validity'
+               });
+               break;
+            }
+         }
+
+         rest();
+      }).catch(function(err) {
+         /* failed to validate the assignedUsers in the database */
+         res.status(500).json({
+            status:'failed',
+            message: 'Internal server error'
+         });
+
+         req.err = 'TaskController.js, Line: 536\nfailed to validate the assignedUsers in the database.\n' + String(err);
+         next();
+      });
+   }
+   else{
+      rest();
+   }
 };
 
 /**
