2c5e0b730284a07927d144c4d3e93b7875a5024b
Kareem Mokhtar <kareem-mokhtar@outlook.com>
2017-03-15 16:19:50 +0200

    reindented the tests

--- test/test.js
+++ test/test.js
@@ -35,25 +35,25 @@ args.chai = require('chai');
 args.chai.use(require('chai-http'));
 
 describe('User Controller', function() {
-    require('./UserController/index')(args);
-    require('./UserController/store')(args);
-    require('./UserController/show')(args);
-    require('./UserController/delete')(args);
+   require('./UserController/index')(args);
+   require('./UserController/store')(args);
+   require('./UserController/show')(args);
+   require('./UserController/delete')(args);
 });
 
 describe('Meeting Controller', function() {
-    require('./MeetingController/store')(args);
-    require('./MeetingController/show')(args);
-    require('./MeetingController/update')(args);
-    require('./MeetingController/delete')(args);
-    require('./MeetingController/rate')(args);
+   require('./MeetingController/store')(args);
+   require('./MeetingController/show')(args);
+   require('./MeetingController/update')(args);
+   require('./MeetingController/delete')(args);
+   require('./MeetingController/rate')(args);
 });
 
 describe('Task Controller', function() {
-    require('./TaskController/store')(args);
-    require('./TaskController/show')(args);
-    require('./TaskController/update')(args);
-    require('./TaskController/delete')(args);
+   require('./TaskController/store')(args);
+   require('./TaskController/show')(args);
+   require('./TaskController/update')(args);
+   require('./TaskController/delete')(args);
 });
 
 describe('Committee Controller', function() {
