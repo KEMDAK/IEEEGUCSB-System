8daac6105acf81e5fc4e84f186acaa2f5c446025
Kareem Mokhtar <kareem-mokhtar@outlook.com>
2017-03-15 16:46:47 +0200

    Update store.js

--- test/UserController/store.js
+++ test/UserController/store.js
@@ -2,7 +2,7 @@ module.exports = function(args) {
    var app, fn, data, models, chai, should;
 
    describe('POST /api/user', function() {
-      this.timeout(20000);    
+      this.timeout(1000);    
       before(function(done) {
          this.timeout(20000);
 
